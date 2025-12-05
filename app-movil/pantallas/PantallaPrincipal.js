c598862be0da3ef9b81752cacb53568e1732b69d
CHARLY120800 <pierolacollazoscharly80@gmail.com>
2025-12-04 19:51:04 -0500

    Update PantallaPrincipal.js

--- app-movil/pantallas/PantallaPrincipal.js
+++ app-movil/pantallas/PantallaPrincipal.js
@@ -1,1 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, FlatList, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import Tarea from '../componentes/Tarea';
 
+export default function PantallaPrincipal({ navigation }) {
+  const [tareas, setTareas] = useState([]);
+
+  const cargarTareas = async () => {
+    const res = await axios.get('http://localhost:3000/tareas');
+    setTareas(res.data);
+  };
+
+  useEffect(() => {
+    cargarTareas();
+  }, []);
+
+  return (
+    <View style={{ padding: 20 }}>
+      <Button title="Agregar Tarea" onPress={() => navigation.navigate('AgregarTarea', { recargar: cargarTareas })} />
+      <FlatList
+        data={tareas}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <Tarea tarea={item} recargar={cargarTareas} />}
+      />
+    </View>
+  );
+}
