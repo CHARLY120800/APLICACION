7f46b1b371c132db275a50614835912e1e147e2f
CHARLY120800 <pierolacollazoscharly80@gmail.com>
2025-12-04 19:47:06 -0500

    Implement task management functions in baseDatos.js

--- backend/baseDatos.js
+++ backend/baseDatos.js
@@ -1,1 +1,9 @@
+let tareas = [];
 
+module.exports = {
+  obtenerTareas: () => tareas,
+  agregarTarea: (tarea) => tareas.push(tarea),
+  eliminarTarea: (id) => {
+    tareas = tareas.filter(t => t.id !== id);
+  }
+};
