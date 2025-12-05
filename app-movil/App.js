a46b35ab43bf337dd3b300a8d5b287b61020c019
CHARLY120800 <pierolacollazoscharly80@gmail.com>
2025-12-04 19:50:29 -0500

    Set up navigation with main and add task screens

--- app-movil/App.js
+++ app-movil/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PantallaPrincipal from './pantallas/PantallaPrincipal';
+import AgregarTarea from './pantallas/AgregarTarea';
 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="PantallaPrincipal">
+        <Stack.Screen name="PantallaPrincipal" component={PantallaPrincipal} />
+        <Stack.Screen name="AgregarTarea" component={AgregarTarea} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
