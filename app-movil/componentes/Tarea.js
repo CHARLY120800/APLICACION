f5c7b7aeff9fb881aa7b5f3cafeb3539ac49f050
CHARLY120800 <pierolacollazoscharly80@gmail.com>
2025-12-04 19:52:39 -0500

    Update Tarea.js

--- app-movil/componentes/Tarea.js
+++ app-movil/componentes/Tarea.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import axios from 'axios';
 
+export default function Tarea({ tarea, recargar }) {
+  const eliminar = async () => {
+    await axios.delete(`http://localhost:3000/tareas/${tarea.id}`);
+    recargar();
+  };
+
+  return (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 5 }}>
+      <Text>{tarea.titulo}</Text>
+      <Button title="Eliminar" onPress={eliminar} />
+    </View>
+  );
+}
